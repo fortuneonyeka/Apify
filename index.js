f57eb9473c824f6a48b003a53696128fac82372f
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-07-14 11:24:02 +0100

    added sorting algorithm

--- index.js
+++ index.js
@@ -3,26 +3,18 @@ const productList = document.createDocumentFragment();
 const url = 'https://api.ecommerce.com/products';
 
 
-
-
-const fetchData = async () => {
-  const response = await fetch("https://6157228e8f7ea600179850e4.mockapi.io/api/vehicles")
-  const data = await response.json()
-  setVehicles(data.sort((v1,v2)=>parseInt(v1.price) < parseInt(v2.price) ? -1: 1))
-}
-
 fetch(url)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      let products = data.sort((mainPrice,maxPrice)=>parseInt(mainPrice.price) < parseInt(maxPrice.price) ? -1: 1);
+      let products = data.sort((minPrice,maxPrice)=>parseInt(minPrice.price) < parseInt(maxPrice.price) ? -1: 1).slice(0,900);
 
       products.map((product)=>{
         let li = document.createElement('li');
         let productName = document.createElement('h2');
-        let productCount = document.createElement('span');
-        let total = document.createElement('span');
+        let productCount = document.createElement('p');
+        let total = document.createElement('p');
 
         productName.textContent = `${product.products.name}`
         productCount.textContent = `${product.count}`
