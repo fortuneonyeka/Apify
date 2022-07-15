5744a7bc6bb936197e70ff0fcd33c7b04202b7f2
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-07-15 06:55:02 +0100

    formatted code

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-const ul = document.getElementById("products")
+const ul = document.getElementById("products");
 const productList = document.createDocumentFragment();
-const url = 'https://api.ecommerce.com/products';
-
+const url = "https://api.ecommerce.com/products";
 
 fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      let products = data.sort((minPrice,maxPrice)=>parseInt(minPrice.price) < parseInt(maxPrice.price) ? -1: 1).slice(0,999);
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    let products = data
+      .sort((minPrice, maxPrice) =>
+        parseInt(minPrice.price) < parseInt(maxPrice.price) ? -1 : 1
+      )
+      .slice(0, 999);
 
-      products.map((product)=>{
-        let li = document.createElement('li');
-        let productName = document.createElement('h2');
-        let productCount = document.createElement('p');
-        let total = document.createElement('p');
+    products.map((product) => {
+      let li = document.createElement("li");
+      let productName = document.createElement("h2");
+      let productCount = document.createElement("p");
+      let total = document.createElement("p");
 
-        productName.textContent = `${product.products.name}`
-        productCount.textContent = `${product.count}`
-        total.textContent = `${product.total}`
+      productName.textContent = `${product.products.name}`;
+      productCount.textContent = `${product.count}`;
+      total.textContent = `${product.total}`;
 
-        li.appendChild(productName);
-        li.appendChild(productCount);
-        li.appendChild(total);
-        list.appendChild(li);
-      })
-    })
-    .catch((error)=>{
-      console.log(error);
-    })
-    ul.appendChild(list);
+      li.appendChild(productName);
+      li.appendChild(productCount);
+      li.appendChild(total);
+      list.appendChild(li);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+ul.appendChild(list);
